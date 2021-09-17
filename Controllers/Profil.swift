c9eaca9104a987005cc6d7ab9c54d0024bbdee76
semmdeveloper <mukhitddinovv23@gmail.com>
2021-09-17 19:03:59 +0500

    Profile view fixed

--- Controllers/Profil.swift
+++ Controllers/Profil.swift
@@ -20,15 +20,17 @@ class Profil: UIViewController {
     }()
 
     let profileView: UIView={
-        var view = UIView()
-        view.frame = CGRect(x: UIScreen.main.bounds.width*0.05, y: UIScreen.main.bounds.height*0.15, width: UIScreen.main.bounds.width*0.90, height: 140)
-        view.layer.cornerRadius = 15
-        view.addSubview(UIImageView(image: UIImage(named: "profile.svg")))
-        view.backgroundColor = UIColor.white
+        var viewframe = UIView()
+        viewframe.frame = CGRect(x: UIScreen.main.bounds.width*0.05, y: UIScreen.main.bounds.height*0.15, width: UIScreen.main.bounds.width*0.90, height: 140)
+        viewframe.layer.cornerRadius = 15
+        let imageView=UIImageView(image: UIImage(named: "profile.svg"))
+        imageView.frame=CGRect(x: viewframe.frame.width*0.35, y: viewframe.frame.height*0.15, width: 110, height: 110)
+        viewframe.addSubview(imageView)
+        viewframe.backgroundColor = UIColor.white
+        return viewframe
         
-        return view
 }()
-    
+
 
     
 
@@ -87,7 +89,18 @@ class Profil: UIViewController {
         lblbtn.title(for: .normal)
         return lblbtn
     }()
-//
+    let imagebtn:UIButton={
+        let imagebtn = UIButton()
+        let imageView=UIImageView(image: UIImage(named: "vec.svg"))
+        imagebtn.frame=CGRect(x: imageView.frame.width*15, y: 255, width: 120, height: 500)
+        imagebtn.addSubview(imageView)
+        imagebtn.setTitleColor(Utils.lblbuttonColor, for: .normal)
+        imagebtn.addTarget(self, action: #selector(editclicked), for: .touchDown)
+        return imagebtn
+    }()
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpUi()
@@ -104,6 +117,10 @@ class Profil: UIViewController {
         view.addSubview(profileView)
         view.addSubview(mailTF)
         view.addSubview(numberTF)
+        view.addSubview(imagebtn)
+
+        
+
 
     }
    @objc func buttonClicked() {
@@ -116,5 +133,9 @@ class Profil: UIViewController {
          
 
      }
+    
+    @objc func editclicked(){
+        print("Button Clicked")
+    }
 
 }
